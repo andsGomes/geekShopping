a74eef6b1b1af67dce06e7570f11c70068f38ff5
andsGomes <anderson.stos.gomes@gmail.com>
2023-11-22 17:44:55 -0300

    Criando VOs e mapiando

--- GeekShopping.ProductAPI/Program.cs
+++ GeekShopping.ProductAPI/Program.cs
@@ -1,3 +1,5 @@
+using AutoMapper;
+using GeekShopping.ProductAPI.Config;
 using GeekShopping.ProductAPI.Model.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
@@ -8,6 +10,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c => 
@@ -20,6 +26,8 @@ builder.Services.AddDbContext<MySqlContext>(options => options
                                                        .UseMySql(connection,
                                                                    new MySqlServerVersion(
                                                                           new Version(8, 2, 0))));
+
+
 
 var app = builder.Build();
 
