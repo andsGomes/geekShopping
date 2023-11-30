39179927fd701d6db0bd6d7870908c943b8f22bd
andsGomes <anderson.stos.gomes@gmail.com>
2023-11-30 15:49:16 -0300

    Crud API completado

--- GeekShopping.ProductAPI/Model/Context/MySqlContext.cs
+++ GeekShopping.ProductAPI/Model/Context/MySqlContext.cs
@@ -4,9 +4,14 @@ namespace GeekShopping.ProductAPI.Model.Context
 {
     public class MySqlContext : DbContext
     {
-        public MySqlContext() {}
+        public MySqlContext() { }
         public MySqlContext(DbContextOptions<MySqlContext> options) : base(options) { }
         public DbSet<Product> Products { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+
 
     }
 }
