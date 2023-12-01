9c7dcee6ed0bdac1c2f81908c93405a9751cfe54
andsGomes <anderson.stos.gomes@gmail.com>
2023-12-01 16:00:05 -0300

    S09 - Algumas Correçoes gerais

--- GeekShopping.ProductAPI/Controllers/ProductController.cs
+++ GeekShopping.ProductAPI/Controllers/ProductController.cs
@@ -39,7 +39,7 @@ namespace GeekShopping.ProductAPI.Controllers
         }
 
         [HttpPut]
-        public async Task<ActionResult<ProductVO>> Update(ProductVO vo)
+        public async Task<ActionResult<ProductVO>> Update([FromBody] ProductVO vo)
         {
             if(vo == null) return BadRequest();
             var product = await _repository.Update(vo);
